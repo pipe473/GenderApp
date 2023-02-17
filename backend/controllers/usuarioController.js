b21ebcfeede65f163b2d814df211ac4f4e22c318
pipe473 <bedoyafelipe743@gmail.com>
2023-02-17 23:21:45 +0100

    Evitando registros duplicados

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,6 +1,15 @@
 import Usuario from '../models/Usuario.js';
 
 const registrar = async (req, res) => {
+    // evitar registros duplicados
+    const { email } = req.body;
+    const existeUsuario = await Usuario.findOne({ email });
+
+    if (existeUsuario) {
+        const error = new Error("Usuario ya registrado");
+        return res.status(400).json({ msg: error.message });
+    }
+    
 
     try {
         const user = new Usuario(req.body);
