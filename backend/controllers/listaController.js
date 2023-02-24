8907a65b09905c0fe88db0618e416d9cb3406696
pipe473 <bedoyafelipe743@gmail.com>
2023-02-24 18:02:46 +0100

    Obteniendo y validando proyectos por su id

--- backend/controllers/listaController.js
+++ backend/controllers/listaController.js
@@ -10,7 +10,6 @@ const nuevaLista = async (req, res) => {
     // console.log(req.body);    
     // console.log(req.usuario);
     const lista = new Lista(req.body);
-
     lista.creador = req.usuario._id;
 
     try {
@@ -22,7 +21,23 @@ const nuevaLista = async (req, res) => {
     
 };
 
-const obtenerLista = async (req, res) => {};
+const obtenerLista = async (req, res) => {
+    const { id } = req.params;
+
+    const lista = await Lista.findById(id);
+
+    if (!lista) {
+       const error = new Error("Lista no encontrada");
+       return res.status(404).json({ msg: error.message });  
+    }   
+
+    if (lista.creador.toString() !== req.usuario._id.toString() ) {
+        const error = new Error("Acción no válida, no tienes los permisos para acceder a esta lista");
+        return res.status(401).json({ msg: error.message });  
+    }  
+
+    res.json(lista);
+};
 
 const editarLista = async (req, res) => {};
 
