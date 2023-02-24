c6a0abb549176f43363962d939a3afd72edf790e
pipe473 <bedoyafelipe743@gmail.com>
2023-02-24 18:24:30 +0100

    Actualizando datos de la lista

--- backend/controllers/listaController.js
+++ backend/controllers/listaController.js
@@ -39,7 +39,33 @@ const obtenerLista = async (req, res) => {
     res.json(lista);
 };
 
-const editarLista = async (req, res) => {};
+const editarLista = async (req, res) => {
+    const { id } = req.params;
+
+    const lista = await Lista.findById(id);
+
+    if (!lista) {
+       const error = new Error("Lista no encontrada");
+       return res.status(404).json({ msg: error.message });  
+    }   
+
+    if (lista.creador.toString() !== req.usuario._id.toString() ) {
+        const error = new Error("Acción no válida, no tienes los permisos para acceder a esta lista");
+        return res.status(401).json({ msg: error.message });  
+    }  
+
+    lista.nombre = req.body.nombre || lista.nombre;
+    lista.descripcion = req.body.descripcion || lista.descripcion;
+    lista.fechaEvento = req.body.fechaEvento || lista.fechaEvento;
+    lista.invitada = req.body.invitada || lista.invitada;
+
+    try {
+        const listaAlmacenada = await lista.save();
+        res.json(listaAlmacenada);
+    } catch (error) {
+        console.log(error);        
+    }
+};
 
 const eliminarLista = async (req, res) => {};
 
