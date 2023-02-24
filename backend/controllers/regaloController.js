8b8a10c0135ee095fd8a2291cd2cf1f4d58fbe99
pipe473 <bedoyafelipe743@gmail.com>
2023-02-24 23:28:35 +0100

    Agregando regalos a una lista existente

--- backend/controllers/regaloController.js
+++ backend/controllers/regaloController.js
@@ -1,4 +1,31 @@
-const agregarRegalo = async (req, res) => {};
+import Lista from '../models/Lista.js';
+import Regalo from '../models/Regalo.js';
+
+const agregarRegalo = async (req, res) => {
+    // console.log(req.body);   
+    const { lista } = (req.body);
+
+    const listaExiste = await Lista.findById(lista);
+
+    if (!listaExiste) {
+        const error = new Error("La lista no existe");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    if (listaExiste.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("No tienes permisos para añadir regalos");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    try {
+        const regaloAlmacenado = await Regalo.create(req.body);
+        res.json(regaloAlmacenado);
+    } catch (error) {
+        console.log(error);        
+    }
+
+//     console.log(listaExiste);    
+};
 
 const obtenerRegalo = async (req, res) => {};
 
@@ -8,7 +35,7 @@ const eliminarRegalo = async (req, res) => {};
 
 const cambiarEstado = async (req, res) => {};
 
-export {
+export { 
     agregarRegalo,
     obtenerRegalo,
     actualizarRegalo,
