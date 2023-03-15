f9c6b38b6b5edb750708bda240ad042dc9b33676
pipe473 <bedoyafelipe743@gmail.com>
2023-03-15 18:00:56 +0100

    Validando token para resetear el password

--- frontend/src/pages/NuevoPassword.jsx
+++ frontend/src/pages/NuevoPassword.jsx
@@ -1,34 +1,68 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { Link, useParams } from "react-router-dom";
+import axios from 'axios';
+import Alerta from '../components/Alerta';
 
 const NuevoPassword = () => {
+
+const [tokenValido, setTokenValido] = useState(false); 
+const [alerta, setAlerta] = useState({})
+
+const params = useParams();
+const { token } = params
+
+
+useEffect(() => {
+  const comprobarToken = async () => {
+    try {
+      await axios(`http://localhost:4500/api/usuarios/olvide-password/${token}`)
+      setTokenValido(true)
+      
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })   
+    }
+  }
+  comprobarToken();
+}, [])
+
+const { msg } = alerta
+
   return (
     <>
       <h1 className="text-teal-500 font-black text-4xl capitalize">
         Recupera tu acceso y sigue{" "}
         <span className="text-teal-700">con tu lista</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
-        <div className="my-5">
-          <label
-            className="uppercase text-teal-700 block text-xl font-bold"
-            htmlFor="password"
-          >
-            Nueva Contraseña
-          </label>
+
+      {msg && <Alerta alerta={alerta} />}
+      
+      { tokenValido && (
+        <form className="my-10 bg-white shadow rounded-lg p-10">
+          <div className="my-5">
+            <label
+              className="uppercase text-teal-700 block text-xl font-bold"
+              htmlFor="password"
+            >
+              Nueva Contraseña
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Escribe tu nueva Contraseña"
+              className="w-full mt-3 p-3 border rounded-xl bg-teal-50"
+            />
+          </div>
           <input
-            id="password"
-            type="password"
-            placeholder="Escribe tu nueva Contraseña"
-            className="w-full mt-3 p-3 border rounded-xl bg-teal-50"
+            type="submit"
+            value="Guardar nueva contraseña"
+            className="bg-teal-700 w-full mb-5 py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-teal-800 trasition-colors"
           />
-        </div>
-        <input
-          type="submit"
-          value="Guardar nueva contraseña"
-          className="bg-teal-700 w-full mb-5 py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-teal-800 trasition-colors"
-        />
       </form>
-      <nav className="lg:flex lg:justify-between">
+      )}
+      {/* <nav className="lg:flex lg:justify-between">
         <Link
           className="block text-center my-5 text-slate-500 uppercase text-sm"
           to="/"
@@ -41,7 +75,7 @@ const NuevoPassword = () => {
         >
           ¿No tienes una cuenta? Regístrate
         </Link>
-      </nav>
+      </nav> */}
     </>
   );
 };
