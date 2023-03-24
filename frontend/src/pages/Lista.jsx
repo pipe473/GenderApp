b1a75f3e476a31400694c79e6cef95e0d26c918b
pipe473 <bedoyafelipe743@gmail.com>
2023-03-25 00:43:38 +0100

    Añadiendo Headless UI

--- frontend/src/pages/Lista.jsx
+++ frontend/src/pages/Lista.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import useListas from '../hooks/useListas';
+import ModalFormularioLista from '../components/ModalFormularioLista';
 
 const Lista = () => {
 
@@ -31,13 +32,15 @@ const Lista = () => {
                 </div>
             </div>
                 <button
-                    type="button"
-                    className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase bg-teal-400 text-white text-center mt-5 flex gap-2 items-center justify-center"
-                >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>                    
-                    Nueva Lista</button>
+                        type="button"
+                        className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase bg-teal-400 text-white text-center mt-5 flex gap-2 items-center justify-center"
+                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>                    
+                    Nueva Lista
+                </button>
+            <ModalFormularioLista />
         </>        
      );
 }
