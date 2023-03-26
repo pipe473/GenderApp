c3a4197ce22b5961cc52809af3fd1c4b0eae895f
pipe473 <bedoyafelipe743@gmail.com>
2023-03-27 00:24:49 +0200

    Solucionar el detalle de la fecha

--- frontend/src/helpers/formatearFecha.jsx
+++ frontend/src/helpers/formatearFecha.jsx
@@ -1,9 +1,5 @@
 export const formatearFecha = fecha => {
-    const nuevaFecha = new Date(fecha)
-
-    console.log(fecha);
-    
-
+    const nuevaFecha = new Date(fecha.split('T')[0].split('-'))  
     const opciones = {
         weekday: 'long',
         year: 'numeric',
